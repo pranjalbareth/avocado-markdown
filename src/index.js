c4fae410a92b9e4397e441b07b2e4fa936715cc4
Pranjal Bareth <pranjalnirmala@gmail.com>
2023-02-26 02:17:23 +0530

    Fixed : Body Tag Theme Change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ThemeProvider } from "./contexts/theme-context";
+import { useTheme, ThemeProvider } from "./contexts/theme-context";
+
+
+function BodyStyles() {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    document.body.style.backgroundColor = theme === 'dark' ? '#000' : '#fff';
+    document.body.style.color = theme === 'dark' ? '#fff' : '#000';
+  }, [theme]);
+
+  return null;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
+      <BodyStyles />
       <App />
     </ThemeProvider>
   </React.StrictMode>
